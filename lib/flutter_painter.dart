e270974c3e4c00f0dc3d46aa17250d980b96b05d
sivanesan <>
2022-04-11 13:07:45 +0530

    Update flutter_painter.dart

--- lib/flutter_painter.dart
+++ lib/flutter_painter.dart
@@ -1,6 +1,6 @@
 /// The Flutter Painter library.
 ///
-/// From this library, you can access all the APIs of Flutter Painter.
+/// you can access all the APIs of Flutter Painter.
 library flutter_painter;
 
 export 'flutter_painter_extensions.dart';
