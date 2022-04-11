8462bcdb4cff837312238a7a21b3c7b977151602
sivanesan <>
2022-04-11 13:47:54 +0530

    extension changes

--- lib/src/views/widgets/flutter_painter.dart
+++ lib/src/views/widgets/flutter_painter.dart
@@ -1,22 +1,23 @@
 import 'dart:async';
 import 'dart:math';
+import 'dart:ui';
 
+import '../../controllers/events/add_text_painter_event.dart';
+import '../../controllers/events/painter_event.dart';
+import '../../controllers/settings/object_settings.dart';
+import '../../controllers/settings/painter_settings.dart';
+import '../../controllers/settings/shape_settings.dart';
+import '../../controllers/settings/text_settings.dart';
+import '../../controllers/settings/haptic_feedback_settings.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import '../../controllers/events/selected_object_drawable_removed_event.dart';
 import '../../controllers/helpers/renderer_check/renderer_check.dart';
-import '../../controllers/drawables/drawable.dart';
+import '../../controllers/drawables/drawables.dart';
 import '../../controllers/notifications/notifications.dart';
 import '../../controllers/drawables/sized1ddrawable.dart';
-import '../../controllers/drawables/shape/shape_drawable.dart';
-import '../../controllers/drawables/sized2ddrawable.dart';
-import '../../controllers/drawables/object_drawable.dart';
-import '../../controllers/events/events.dart';
-import '../../controllers/drawables/text_drawable.dart';
-import '../../controllers/drawables/path/path_drawables.dart';
-import '../../controllers/settings/settings.dart';
 import '../painters/painter.dart';
 import '../../controllers/painter_controller.dart';
 import '../../controllers/helpers/border_box_shadow.dart';
@@ -25,13 +26,17 @@ import 'painter_controller_widget.dart';
 import 'dart:math' as math;
 
 part 'free_style_widget.dart';
-part 'text_widget.dart';
 part 'object_widget.dart';
+part 'text_widget.dart';
 part 'shape_widget.dart';
 
 typedef DrawableCreatedCallback = Function(Drawable drawable);
 
+typedef DrawableUpdatedCallback = Function(Drawable drawable);
+
 typedef DrawableDeletedCallback = Function(Drawable drawable);
+
+typedef DrawableTappedCallback = Function(Drawable drawable);
 
 /// Defines the builder used with [FlutterPainter.builder] constructor.
 typedef FlutterPainterBuilderCallback = Widget Function(
@@ -45,8 +50,14 @@ class FlutterPainter extends StatelessWidget {
   /// Callback when a [Drawable] is created internally in [FlutterPainter].
   final DrawableCreatedCallback? onDrawableCreated;
 
+  /// Callback when a [Drawable] is updated internally in [FlutterPainter].
+  final DrawableDeletedCallback? onDrawableUpdated;
+
   /// Callback when a [Drawable] is deleted internally in [FlutterPainter].
   final DrawableDeletedCallback? onDrawableDeleted;
+
+  /// Callback when a [Drawable] is deleted internally in [FlutterPainter].
+  final DrawableTappedCallback? onDrawableTapped;
 
   /// Callback when the selected [ObjectDrawable] changes.
   final ValueChanged<ObjectDrawable?>? onSelectedObjectDrawableChanged;
@@ -67,7 +78,9 @@ class FlutterPainter extends StatelessWidget {
       {Key? key,
       required this.controller,
       this.onDrawableCreated,
+      this.onDrawableUpdated,
       this.onDrawableDeleted,
+      this.onDrawableTapped,
       this.onSelectedObjectDrawableChanged,
       this.onPainterSettingsChanged})
       : _builder = _defaultBuilder,
@@ -82,7 +95,9 @@ class FlutterPainter extends StatelessWidget {
       required this.controller,
       required FlutterPainterBuilderCallback builder,
       this.onDrawableCreated,
+      this.onDrawableUpdated,
       this.onDrawableDeleted,
+      this.onDrawableTapped,
       this.onSelectedObjectDrawableChanged,
       this.onPainterSettingsChanged})
       : _builder = builder,
@@ -101,7 +116,9 @@ class FlutterPainter extends StatelessWidget {
                   key: controller.painterKey,
                   controller: controller,
                   onDrawableCreated: onDrawableCreated,
+                  onDrawableUpdated: onDrawableUpdated,
                   onDrawableDeleted: onDrawableDeleted,
+                  onDrawableTapped: onDrawableTapped,
                   onPainterSettingsChanged: onPainterSettingsChanged,
                   onSelectedObjectDrawableChanged:
                       onSelectedObjectDrawableChanged,
@@ -124,8 +141,14 @@ class _FlutterPainterWidget extends StatelessWidget {
   /// Callback when a [Drawable] is created internally in [FlutterPainter].
   final DrawableCreatedCallback? onDrawableCreated;
 
+  /// Callback when a [Drawable] is created internally in [FlutterPainter].
+  final DrawableUpdatedCallback? onDrawableUpdated;
+
   /// Callback when a [Drawable] is deleted internally in [FlutterPainter].
   final DrawableDeletedCallback? onDrawableDeleted;
+
+  /// Callback when a [Drawable] is deleted internally in [FlutterPainter].
+  final DrawableTappedCallback? onDrawableTapped;
 
   /// Callback when the selected [ObjectDrawable] changes.
   final ValueChanged<ObjectDrawable?>? onSelectedObjectDrawableChanged;
@@ -138,7 +161,9 @@ class _FlutterPainterWidget extends StatelessWidget {
       {Key? key,
       required this.controller,
       this.onDrawableCreated,
+      this.onDrawableUpdated,
       this.onDrawableDeleted,
+      this.onDrawableTapped,
       this.onSelectedObjectDrawableChanged,
       this.onPainterSettingsChanged})
       : super(key: key);
@@ -165,8 +190,6 @@ class _FlutterPainterWidget extends StatelessWidget {
                       (controller.freeStyleSettings.mode == FreeStyleMode.none),
                   scaleEnabled: controller.settings.scale.enabled,
                   child: _FreeStyleWidget(
-                      // controller: controller,
-                      child: _TextWidget(
                     // controller: controller,
                     child: _ShapeWidget(
                       // controller: controller,
@@ -181,7 +204,7 @@ class _FlutterPainterWidget extends StatelessWidget {
                         ),
                       ),
                     ),
-                  )),
+                  ),
                 ),
               );
             }));
@@ -191,8 +214,12 @@ class _FlutterPainterWidget extends StatelessWidget {
   bool onNotification(FlutterPainterNotification notification) {
     if (notification is DrawableCreatedNotification) {
       onDrawableCreated?.call(notification.drawable);
+    } else if (notification is DrawableUpdatedNotification) {
+      onDrawableUpdated?.call(notification.drawable);
     } else if (notification is DrawableDeletedNotification) {
       onDrawableDeleted?.call(notification.drawable);
+    } else if (notification is DrawableTapNotification) {
+      onDrawableTapped?.call(notification.drawable);
     } else if (notification is SelectedObjectDrawableUpdatedNotification) {
       onSelectedObjectDrawableChanged?.call(notification.drawable);
     } else if (notification is SettingsUpdatedNotification) {
